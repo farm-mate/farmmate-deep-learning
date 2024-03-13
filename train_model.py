07cb9a7c6a855f65981c8b67403ed93a03428e98
YoujeongChoi <yj91322@gmail.com>
2024-03-13 20:21:14 +0900

    fix: 모델 가중치 설정

--- train_model.py
+++ train_model.py
@@ -97,6 +97,7 @@ class CustomDataset(Dataset):
         #     print(f"라벨: {label}")
         #     self.printed_labels.add(label)
 
+        # TODO : 작물에 맞게 수정
         if label['disease'] == 16:  # 참외노균병
             new_label = 1
         elif label['disease'] == 17:    #참외흰가루병
@@ -211,7 +212,53 @@ def train_resnet(model, criterion, optimizer, scheduler, num_epochs=25):
 # 전이 학습 모델 로드 및 설정
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {DEVICE}")
-criterion = nn.CrossEntropyLoss()
+
+# TODO : 작물별 가중치
+# 클래스 가중치 설정
+# class_weights = torch.tensor([1.0, 12.0, 12.0], dtype=torch.float).to(DEVICE)  # '정상', '질병1', '질병2'의 가중치
+
+## 가지
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 고추
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 토마토
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 수박
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 참외
+class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 딸기
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 상추
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 가지
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 단호박
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 쥬키니호박
+# class_weights = torch.tensor([1.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 오이
+# class_weights = torch.tensor([1.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 애호박
+# class_weights = torch.tensor([1.0, 10.0, 10.0], dtype=torch.float).to(DEVICE)
+
+## 포도
+# class_weights = torch.tensor([1.0, 10.0], dtype=torch.float).to(DEVICE)
+
+criterion = nn.CrossEntropyLoss(weight=class_weights)
+# criterion = nn.CrossEntropyLoss()
+
 resnet = models.resnet50(pretrained=True)
 for param in resnet.parameters():
     param.requires_grad = False
@@ -228,5 +275,6 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 EPOCH = 30
 model_resnet50 = train_resnet(resnet, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=EPOCH)
 
+# TODO : 작물에 따라 이름 수정
 # 모델 저장
-torch.save(model_resnet50.state_dict(), 'melon_resnet50.pth')
+torch.save(model_resnet50.state_dict(), '참외_resnet50.pth')
